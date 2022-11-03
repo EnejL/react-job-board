84296b2efd0107e9a868e7dac04a7f89d8639567
Enej Licina <elicina@nbeyond.nl>
2022-11-03 16:37:52 +0100

    first commit

---
+++ src/components/JobListing.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from "../logo.svg";
+
+function JobListing() {
+    return (
+        <div className="JobListings">
+            <div className="JobListing">
+                This is a job listing
+            </div>
+            <div className="JobListing">
+                This is a job listing
+            </div>
+            <div className="JobListing">
+                This is a job listing
+            </div>
+            <div className="JobListing">
+                This is a job listing
+            </div>
+            <div className="JobListing">
+                This is a job listing
+            </div>
+            <div className="JobListing">
+                This is a job listing
+            </div>
+            <div className="JobListing">
+                This is a job listing
+            </div>
+            <div className="JobListing">
+                This is a job listing
+            </div>
+            <div className="JobListing">
+                This is a job listing
+            </div>
+            <div className="JobListing">
+                This is a job listing
+            </div>
+        </div>
+    );
+}
+
+export default JobListing;
