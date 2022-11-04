88faec6bde2de8d23e95565ad0e9d3d31c200dbe
Enej Licina <elicina@nbeyond.nl>
2022-11-04 13:50:06 +0100

    minor layout fixes

--- src/components/JobListing.js
+++ src/components/JobListing.js
@@ -2,39 +2,13 @@ import React from 'react';
 import logo from "../logo.svg";
 
 function JobListing() {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const listItems = numbers.map((number) =>
+        <li className="JobListing">This is a job listing</li>
+    );
+
     return (
-        <div className="JobListings">
-            <div className="JobListing">
-                This is a job listing
-            </div>
-            <div className="JobListing">
-                This is a job listing
-            </div>
-            <div className="JobListing">
-                This is a job listing
-            </div>
-            <div className="JobListing">
-                This is a job listing
-            </div>
-            <div className="JobListing">
-                This is a job listing
-            </div>
-            <div className="JobListing">
-                This is a job listing
-            </div>
-            <div className="JobListing">
-                This is a job listing
-            </div>
-            <div className="JobListing">
-                This is a job listing
-            </div>
-            <div className="JobListing">
-                This is a job listing
-            </div>
-            <div className="JobListing">
-                This is a job listing
-            </div>
-        </div>
+        <ul className="JobListings">{listItems}</ul>
     );
 }
 
